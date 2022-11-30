c878bab1615c6a011528d01ac16ac12efd9fa6a5
Andrea-Baggio <andreabaggio800@gmail.com>
2022-11-30 16:19:26 +0100

    css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="body">
+    <MainPageVue />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import MainPageVue from './components/MainPage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    MainPageVue,
   },
 };
 </script>
-
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+.body {
+  background-color: gray;
+  color: white;
+  font-family: 'Lato', sans-serif;
+}
+
 </style>
